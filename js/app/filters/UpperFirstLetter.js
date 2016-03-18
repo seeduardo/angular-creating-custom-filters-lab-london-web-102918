b8d352cf8e3aac3b42787a14aa261b3d07289c7d
ipc103 <iancandysemail@gmail.com>
2016-03-18 14:14:59 -0400

    remove solution

--- js/app/filters/UpperFirstLetter.js
+++
@@ -1,9 +0,0 @@
-function UpperFirstLetter() {
-	return function (str) {
-		return str[0].toUpperCase() + str.slice(1);
-	}
-}
-
-angular
-	.module('app')
-	.filter('upperFirstLetter', UpperFirstLetter);
